7d32072124ef4c453dd3de8c3d718812dee6f797
sarahf2 <sarahf2trial@gmail.com>
2022-06-16 05:54:34 +1000

    [sarah]

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-import rhino3dm import *
+from rhino3dm import *
 
-app=Flask(_name_)
+app=Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	return 'Hello world from Sarah UNSW CODE!'
 
-	
