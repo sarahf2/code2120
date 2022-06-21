70717c58695cdfc8373765521a18b3171382a214
sarahf2 <sarahf2trial@gmail.com>
2022-06-21 23:15:00 +1000

    [website]

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from rhino3dm import *
+from openpyxl.workbook import Workbook
+
+wb=Workbook()
+ws=wb.active
+
+ws.title="Week 4 website"
+
+ws['A1']='Welcome!'
+c=ws['A1']
+print(c.value)
+
+wb.save("Week 4.xlsx")
+
+###
 
 app=Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello world from Sarah UNSW CODE!'
+	return c.value
 
